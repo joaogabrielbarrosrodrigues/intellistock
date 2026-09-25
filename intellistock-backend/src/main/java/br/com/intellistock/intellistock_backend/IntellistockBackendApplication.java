aac1c63c8b7f2098a2f864719a1afd6c52787d5f
joaogabrielbarrosrodrigues <joaogabrielbarrosrodrigues@gmail.com>
2026-09-25 12:11:41 -0300

    Estrutura inicial do backend - spring boot

---
+++ intellistock-backend/src/main/java/br/com/intellistock/intellistock_backend/IntellistockBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.intellistock.intellistock_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntellistockBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntellistockBackendApplication.class, args);
+	}
+
+}
---
+++ intellistock-backend/src/test/java/br/com/intellistock/intellistock_backend/IntellistockBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.intellistock.intellistock_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntellistockBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
